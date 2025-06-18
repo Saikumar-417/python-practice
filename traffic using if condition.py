ab5da571f43ed0450beb6bc55b69d1d7d4b56329
Saikumar-417 <maradasaidkumar@gmail.com>
2025-06-18 23:06:07 +0530

    Add files via upload

---
+++ traffic using if condition.py
@@ -0,0 +1,14 @@
+str =input("enter color:-")
+if str=="green":
+    print("go")
+    speed = int(input("enter speed:"))
+    if speed>80 and True:
+        print("pay fine upto 5000 for over speed and not helmet fine 1000")
+    elif speed>=50 and True:
+        print("pay fine 1000 for no helmet")
+    else:
+        print("go safe")
+elif str=="yellow":
+    print("ready to go")
+else:
+    print("stop")
