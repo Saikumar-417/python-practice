58802935f3f1893948130104378b2e9dc529f13e
Marada Sai kumar <maradasaidkumar@gmail.com>
2025-06-25 15:15:23 +0530

    Add files via upload

---
+++ function basic login program.py
@@ -0,0 +1,18 @@
+name ="sai"
+age= 22
+role="developer"
+print(name)
+print(age)
+print(role)
+def login():
+    email="sai@gmail.com"
+    password=123456
+    e=(input("enter email:"))
+    p=int(input("enter password:"))
+    if email==e and password==p:
+        print("you succesfully login")
+    else:
+        print("invalid credentials")
+print("hello")
+print("hello2")
+login()
