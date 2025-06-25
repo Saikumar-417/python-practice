c3b68732b3a1e20dac8e99e471598712dcec0be6
Marada Sai kumar <maradasaidkumar@gmail.com>
2025-06-25 15:01:24 +0530

    Add files via upload

---
+++ function basic program2.py
@@ -0,0 +1,10 @@
+def add(): 
+    first=int(input("enter first number:--"))
+    second=int(input("enter second number:--"))
+    print("hello")
+    a=29
+    b=24
+    c=a+b
+    print(c)
+    print(first+second)
+add()
