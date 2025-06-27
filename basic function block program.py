8bc9e4a0333c2a130bf683113470a7a35468327b
Marada Sai kumar <maradasaidkumar@gmail.com>
2025-06-27 12:10:47 +0530

    Add files via upload

---
+++ basic function block program.py
@@ -0,0 +1,11 @@
+hero="RAVI TEJA"
+director="RAJAMOULI"
+movie="VIKRAMARKUDU"
+
+def moviedetails():
+    hero="ravi teja"
+    director="rajamouli"
+    movie="vikramarkudu"
+    print(f"hey this is {hero} movie with {director} and the movie name is {movie}")
+moviedetails()
+print(f"hey this is {hero} movie with {director} and the movie name is {movie}")
---
+++ ruturn keyword in function basic program .py
@@ -0,0 +1,12 @@
+age=22
+name="sai"
+loc="vizag"
+role="developer"
+def add():
+    a=10
+    b=20
+    print(a+b)
+    return a+b , a-b
+
+result=add()
+print(result)
