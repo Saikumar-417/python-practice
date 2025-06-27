a2ab4fd529136872db2ca53aa8eb5cfff4a35dfe
Marada Sai kumar <maradasaidkumar@gmail.com>
2025-06-27 12:34:54 +0530

    Add files via upload

---
+++ basic program in function using two blocks.py
@@ -0,0 +1,15 @@
+country_name ="india"
+pm ="narendra modi"
+population ="1.5 billion"
+
+def countrydetails():
+    country_name ="russia"
+    pm ="putin"
+    
+    def armydetails():
+        army_count ="30 lacks"
+        print(f"{country_name} {pm} {army_count}" )
+    armydetails()
+countrydetails()
+army_count="35 lacks"
+print(f"{ country_name} {pm} {army_count} {population}" )
