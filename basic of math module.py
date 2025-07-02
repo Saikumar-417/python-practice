c29aa4de0b76261866899afb061f771c2c0d2b21
Marada Sai kumar <maradasaidkumar@gmail.com>
2025-07-02 22:13:27 +0530

    Add files via upload

---
+++ basic of math module.py
@@ -0,0 +1,25 @@
+#built in modules - math
+#sqrt
+import math
+a=int(math.sqrt(16))
+print(a)
+
+#pow
+import math
+a=int(math.pow(2,3))
+print(a)
+
+#ceil
+import math
+b=math.ceil(3.001)
+print(b)
+
+#floor
+import math
+c=math.floor(4.567)
+print(c)
+
+#fabs
+import math
+a=math.fabs(-100)
+print(a)
